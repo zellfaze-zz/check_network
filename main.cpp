a125b971f7b3508d1fcf20d7a3a35cb7b65bb588
Derric Atzrott <zellfaze@zellfaze.org>
2016-10-31 01:48:19 -0400

    Split code into functions
    This code should be much easier to work with now that it has been
    split into individual functions.

--- main.cpp
+++ main.cpp
@@ -10,26 +10,21 @@ inline bool exists(const std::string& name) {
   return (stat (name.c_str(), &buffer) == 0);
 }
 
+void toggleTmp(string s_tmpFile);
+string callPing(string s_pingTmpFile);
+void findDirectories(char* c_tmpDir, string* s_tmpFile, string* s_pingTmpFile);
+
 int main(int argc, char *argv[])
 {
     bool toggleMode = false;
-
-    //Determine tmp location
     char* c_tmpDir;
-    char* c_username;
-    char c_defaultTmpDir[] = {'/', 't', 'm', 'p', 0};
-    c_tmpDir = getenv("TMPDIR");
-    c_username = getenv("USER");
-    string s_tmpFile = "net_check";
-    string s_pingTmpFile = "net_check_ping";
+    string(s_tmpFile);
+    string(s_pingTmpFile);
 
-    if (c_tmpDir == 0)
-        c_tmpDir = c_defaultTmpDir;
+    //Find tmp directory
+    findDirectories(c_tmpDir, &s_tmpFile, &s_pingTmpFile);
 
-    s_tmpFile = string(c_tmpDir) + "/" + string(c_username) + s_tmpFile;
-    s_pingTmpFile = string(c_tmpDir) + "/" + string(c_username) + s_pingTmpFile;
-
-    //Toggle mode?
+    //Check options
     if (argc > 1) {
         if (string(argv[1]) == "-t")
             toggleMode = true;
@@ -40,44 +35,65 @@ int main(int argc, char *argv[])
     }
 
     if (toggleMode) {
-
-
-        //Check if tmp file exists
-        if (exists(s_tmpFile)) {
-            //Delete the tmp file
-            remove(s_tmpFile.c_str());
-        } else {
-            //Create the tmp file
-            ofstream o_tmp(s_tmpFile.c_str());
-            o_tmp << '\0';
-            o_tmp.close();
-        }
-
+        toggleTmp(s_tmpFile);
     } else {
         //Check if we should call ping
-        if (exists(s_tmpFile)) {
-            //Call our ping command
-            string s_pingCmd = "ping -c 1 -w 4 8.8.8.8 | grep time= | sed 's/.*time=//' > " + s_pingTmpFile;
-            string s_rmCmd = "rm " + s_pingTmpFile;
-            system(s_pingCmd.c_str());
-
-            //Get the results
-            string line;
-            ifstream results(s_pingTmpFile);
-            if (results.is_open()) {
-                getline(results,line);
-                results.close();
-                system(s_rmCmd.c_str());
-            } else
-                line = "Error";
-
-            if (line.size() == 0)
-                cout << "Disconnected";
-             else
-                cout << line;
-        } else
+        if (exists(s_tmpFile))
+            cout << callPing(s_pingTmpFile);
+        else
             cout << "Off";
     }
 
     return 0;
+}
+
+void toggleTmp(string s_tmpFile) {
+    //Check if tmp file exists
+    if (exists(s_tmpFile)) {
+        //Delete the tmp file
+        remove(s_tmpFile.c_str());
+    } else {
+        //Create the tmp file
+        ofstream o_tmp(s_tmpFile.c_str());
+        o_tmp << '\0';
+        o_tmp.close();
+    }
+}
+
+string callPing(string s_pingTmpFile) {
+    //Call our ping command
+    string s_pingCmd = "ping -c 1 -w 4 8.8.8.8 | grep time= | sed 's/.*time=//' > " + s_pingTmpFile;
+    string s_rmCmd = "rm " + s_pingTmpFile;
+    system(s_pingCmd.c_str());
+
+    //Get the results
+    string line;
+    ifstream results(s_pingTmpFile.c_str());
+    if (results.is_open()) {
+        getline(results,line);
+        results.close();
+        system(s_rmCmd.c_str());
+    } else
+        line = "Error";
+
+    if (line.size() == 0)
+        line = "Disconnected";
+
+    return line;
+}
+
+void findDirectories(char* c_tmpDir, string* s_tmpFile, string* s_pingTmpFile) {
+    //Determine tmp location
+    char* c_username;
+    char c_defaultTmpDir[] = {'/', 't', 'm', 'p', 0};
+    c_tmpDir = getenv("TMPDIR");
+    c_username = getenv("USER");
+    *s_tmpFile = "net_check";
+    *s_pingTmpFile = "net_check_ping";
+
+    if (c_tmpDir == 0)
+        c_tmpDir = c_defaultTmpDir;
+
+    *s_tmpFile = string(c_tmpDir) + "/" + string(c_username) + *s_tmpFile;
+    *s_pingTmpFile = string(c_tmpDir) + "/" + string(c_username) + *s_pingTmpFile;
 }
